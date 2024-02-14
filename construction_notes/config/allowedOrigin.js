d356586c391eb06795b74863e02c4ef11fed932b
Vishal Giri <vishalgiri197@gmail.com>
2024-02-14 15:44:43 +0530

    corsAdded

--- construction_notes/config/allowedOrigin.js
+++ construction_notes/config/allowedOrigin.js
@@ -1,6 +1,4 @@
 const allowedorigins = [
-"http://localhost:3000",
-   "https://consnotes-2n8ske0ry-vishalg197.vercel.app",
-   "https://consnotes.vercel.app/"
+ "https://consnotes.vercel.app"
 ]
 module.exports = allowedorigins;
--- technotes/src/app/api/apiSlice.js
+++ technotes/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://construction-5yqt.onrender.com',
+    baseUrl: 'https://vishalnotes.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
